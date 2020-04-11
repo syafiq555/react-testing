1e3ca76a38fd2109ef2fa581cc4fca0ce7ea9681
syafiqshamsuddin96 <syafiqshamsuddin96@gmail.com>
2020-04-12 00:53:25 +0800

    setup test utils to use redux

--- src/utils/testUtils/testUtils.jsx
+++ src/utils/testUtils/testUtils.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 
-const AllProviders = ({ children }) => {
-  return <div>{children}</div>;
+import configureStore from '../../redux/index';
+
+const customRender = (ui, { initialState = {}, _store, ...options } = {}) => {
+  const store = _store || configureStore(initialState);
+  const Wrapper = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  Wrapper.propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+    ]).isRequired,
+  };
+
+  return render(ui, {
+    wrapper: Wrapper,
+    ...options,
+  });
 };
-
-AllProviders.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]).isRequired,
-};
-
-const customRender = (ui, options) =>
-  render(ui, { wrapper: AllProviders, ...options });
 
 export * from '@testing-library/react';
 
