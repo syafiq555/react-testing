b77ef946c10470392978a0b8191d5a71500889bc
syafiqshamsuddin96 <syafiqshamsuddin96@gmail.com>
2020-04-12 01:29:38 +0800

    delete todo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import generateId from '../../utils/generateId';
-import { addTodo } from '../../redux/todos';
+import { addTodo, deleteTodo } from '../../redux/todos';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -15,20 +15,28 @@ const App = () => {
     dispatch(addTodo(value));
     todoRef.current.value = '';
   };
+  const onRemoveTodo = (todo) => () => dispatch(deleteTodo(todo));
 
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="todo">
-          Todo
-          <textarea ref={todoRef} type="text" id="todo" />
-        </label>
-        <button type="submit">Add</button>
-      </form>
+      <div>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="todo">
+            Todo
+            <textarea ref={todoRef} type="text" id="todo" />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
       <div>
         <ul>
           {todos.map((todo) => (
-            <li key={generateId()}>{todo}</li>
+            <li key={generateId()}>
+              <span>{todo}</span>
+              <button onClick={onRemoveTodo(todo)} type="button">
+                delete
+              </button>
+            </li>
           ))}
         </ul>
       </div>
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 
-import { render, fireEvent } from '../../utils/testUtils';
+import { render, fireEvent, cleanup } from '../../utils/testUtils';
 import App from './App';
 
-test('Add to do', async () => {
-  const { getByLabelText, getByText } = render(<App />);
-  const text = 'IDK ✅';
-  const textInput = getByLabelText(/todo/i);
-  const addButton = getByText(/add/i);
-  fireEvent.change(textInput, {
-    target: { value: text },
+afterEach(cleanup);
+
+describe('Todo', () => {
+  let container;
+  beforeEach(() => {
+    container = render(<App />);
   });
-  expect(textInput).toHaveValue(text);
-  fireEvent.click(addButton);
-  expect(getByText(text)).toBeInTheDocument();
-  expect(textInput).toHaveValue('');
+  test('Add to do', async () => {
+    const { getByLabelText, getByText } = container;
+    const text = 'IDK ✅';
+    const textInput = getByLabelText(/todo/i);
+    const addButton = getByText(/add/i);
+    fireEvent.change(textInput, {
+      target: { value: text },
+    });
+    expect(textInput).toHaveValue(text);
+    fireEvent.click(addButton);
+    expect(getByText(text)).toBeInTheDocument();
+    expect(textInput).toHaveValue('');
+  });
+
+  test('Should render with a ready todo', async () => {
+    const todos = ['asdadas', 'asdas', 'sss'];
+    const { getByText } = render(<App />, {
+      initialState: { todos },
+    });
+    todos.forEach((todo) => expect(getByText(todo)).toBeInTheDocument());
+  });
+
+  test('delete todo', async () => {
+    const todos = ['asdadas', 'asdas', 'sss'];
+    const { queryByText } = render(<App />, {
+      initialState: { todos },
+    });
+    const todoToDelete = todos[0];
+    const parent = queryByText(todoToDelete).parentElement;
+    const deleteButton = parent.querySelector('button');
+    expect(deleteButton).not.toBeNull();
+    fireEvent.click(deleteButton);
+    expect(parent).not.toBeInTheDocument();
+  });
 });
